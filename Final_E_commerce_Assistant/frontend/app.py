2665cc763c4fa138b48513c7cf3484811c07b863
Shivpriya Mane <mane.shi@northeastern.edu>
2025-12-07 16:55:20 -0500

    update

--- Final_E_commerce_Assistant/frontend/app.py
+++ Final_E_commerce_Assistant/frontend/app.py
@@ -6,7 +6,7 @@ import json
 import time
 
 #BACKEND_URL = "http://127.0.0.1:8000"
-BACKEND_URL = "https://partnerships-describing-seq-gather.trycloudflare.com"
+BACKEND_URL = "https://lodging-governmental-job-vacancies.trycloudflare.com"
 st.set_page_config(
     page_title="Smart E-Commerce Assistant",
     page_icon="🛒",
